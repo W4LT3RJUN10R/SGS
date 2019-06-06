5f09208d097620c338c12a259f3103b12d525f4c
prof barba <bernardodesousa@gmail.com>
2019-06-06 16:08:04 -0300

    Solve 4.4

--- exercicios/4.4.sql
+++ exercicios/4.4.sql
@@ -1,3 +1,11 @@
 /*
 4.4 Criar uma visão com o custo total das manutenções realizadas para cada cliente. Considere apenas as solicitações atendidas. Esta visão é atualizável? Porque?
 */
+
+CREATE OR REPLACE VIEW CustoPorCliente AS
+SELECT Solicitacao.cliente, SUM(Ocorrencia.duracao * Categoria.precoHora) as custo
+FROM Categoria INNER JOIN Solicitacao ON Solicitacao.categoria = Categoria.codigo
+INNER JOIN Ocorrencia ON Ocorrencia.solicitacao = Solicitacao.codigo
+WHERE Solicitacao.situacao = 3 GROUP BY Solicitacao.cliente;
+
+SELECT * FROM CustoPorCliente;
