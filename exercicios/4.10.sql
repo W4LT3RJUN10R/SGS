dd461f5801c6e2b379da389e30637a199145c0d5
prof barba <bernardodesousa@gmail.com>
2019-06-10 16:36:50 -0300

    Solve 4.10

---
+++ exercicios/4.10.sql
@@ -0,0 +1,10 @@
+/*
+4.10 Explique para que serve a claúsula group by e dê 1 exemplo de sua utilização.
+*/
+
+/*
+A cláusula GROUP BY serve para regrupar as linhas de uma tabela em conjuntos que tem algo em comum. Aquilo que as linhas agrupadas tem em comum é o conteúdo da coluna informada logo após o comando GROUP BY. Por exemplo, no comando SQL abaixo, agrupamos as linhas da tabela Ocorrencia por técnico, de forma que subconjuntos de occorências, com o técnico em comum, são criados em memória principal. Projetamos o código do técnico (Ocorrencia.tecnico) e o número de ocorrências em cada subconjunto.
+*/
+
+SELECT Ocorrencia.tecnico, COUNT(*) FROM Ocorrencia
+GROUP BY Ocorrencia.tecnico;
