7d120d1144b8ab65d20cd7ae87175c0aaa006c1b
prof barba <bernardodesousa@gmail.com>
2019-06-10 20:45:07 -0300

    Solve 4.19

--- exercicios/4.19.sql
+++ exercicios/4.19.sql
@@ -1,9 +1,11 @@
 /*
-4.18 Dê exemplo de uma consulta utilizando a cláusula MINUS
+4.19 Dê exemplo de uma consulta utilizando a cláusula INTERSECT.
 */
 
 /*
-A consulta abaixo apresenta os códigos das solicitações do cliente 5 que não dizem respeito ao produto 19. No dataset deste trabalho, há 5 solicitações do cliente 5 e 12 solicitações para o produto 19. Com a cláusula MINUS, podemos eliminar do resultado a interceção entre esses dois conjuntos.
+    As mesmas seleções do exercício 4.18 são utilizadas aqui para demonstrar a cláusula INTERSECT. Nota-se que a obrigatoriedade de operar sobre tabelas de estrutura idência é compartilhada entre estes dois recusos.
+    O resultado neste caso é a seleção da interseção entre o conjunto de solicitações do cliente 5 e o conjunto de solicitações sobre o produto 19.
+    No dataset destre trabalho, apenas duas linhas pertencem a interseção
 */
 
 SELECT * FROM (
@@ -12,9 +14,9 @@ SELECT * FROM (
     INNER JOIN Cliente
     ON Solicitacao.cliente = Cliente.codigo
     WHERE Cliente.codigo = 5
-
-    MINUS
-
+    
+    INTERSECT
+    
     SELECT Solicitacao.codigo as solicitacao, Solicitacao.produto, Solicitacao.cliente FROM Solicitacao INNER JOIN Produto
     ON Solicitacao.produto = Produto.codigo
     INNER JOIN Cliente
