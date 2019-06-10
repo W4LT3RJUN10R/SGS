b8af9eb04fd5e8eb600fded675d79a84c938bfd0
prof barba <bernardodesousa@gmail.com>
2019-06-10 16:25:51 -0300

    Solve 4.9

--- exercicios/4.9.sql
+++ exercicios/4.9.sql
@@ -2,3 +2,10 @@
 4.9 Acrescente uma coluna nova “data de inclusão” no formato date, na primeira tabela criada. Altere o valor desta coluna colocando a data do sistema.
 */
 
+/* Comentários
+    "na primeira tabela criada"?
+    No nosso caso, a primeira tabela criada foi Tipo. Decidimos alterar a tabela Cliente, no lugar de Tipo, por nos parecer que, para um sistema como este, registrar a data de inserção de um cliente é mais útil do que a dos tipos recorrentes de problemas.
+*/
+
+ALTER TABLE Cliente ADD dataDeInclusao DATE;
+UPDATE Cliente SET dataDeInclusao = SYSDATE;
