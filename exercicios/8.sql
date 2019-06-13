231e1681407a5281cdd29cf507353e56a50373cd
W4LT3RJUN10R <46505119+W4LT3RJUN10R@users.noreply.github.com>
2019-06-12 22:21:14 -0300

    Meu Exercício 8

--- exercicios/8.sql
+++ exercicios/8.sql
@@ -1,16 +1,23 @@
-CREATE OR REPLACE PROCEDURE custoManutencao(pCod Solicitacao.codigo%TYPE)
+CREATE OR REPLACE PROCEDURE CustoTotalManutencao(pCod Solicitacao.codigo%TYPE)
+
 AS
-custoHora Categoria.precoHora%TYPE;
-totalDuracao NUMBER;
-cTotal NUMBER(8, 2);
-BEGIN
-    SELECT precoHora INTO custoHora FROM Categoria
-    WHERE codigo = (SELECT categoria FROM Solicitacao WHERE codigo = pCod);
 
-    SELECT SUM(Ocorrencia.duracao) INTO totalDuracao FROM Ocorrencia WHERE solicitacao = pCod;
+TotalDuracao NUMBER;
 
-    cTotal := custoHora * totalDuracao;
+CustoHora Categoria.precoHora%TYPE;
 
-    UPDATE Solicitacao SET custoTotal = cTotal WHERE pCod = Solicitacao.codigo;
-END custoManutencao;
-/
+CustoTotal NUMBER(8, 2);
+
+	BEGIN
+
+		SELECT precoHora INTO custoHora FROM Categoria
+	
+		WHERE codigo = (SELECT categoria FROM Solicitacao WHERE codigo = pCod);
+
+		SELECT SUM(Ocorrencia.duracao) INTO TotalDuracao FROM Ocorrencia WHERE solicitacao = pCod;
+
+		CustoTotal := totalDuracao * CustoHora;
+
+		UPDATE Solicitacao SET custoTotal = CustoTotal WHERE pCod = Solicitacao.codigo;
+
+	END CustoTotalManutencao;/
